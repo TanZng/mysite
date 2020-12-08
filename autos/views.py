d1b5e3f1cd1f07388bbdbb43ff745f8f7257e575
Tan <taniazuniga.mx@gmail.com>
2020-12-07 20:11:06 -0600

    Added Autos application
    + autos\models.py
    For Autos and Make (manufacturer of the car)
    
    + autos/urls.py
    main CRUD for autos/car
    lookup CRUD for make/manufacturer
    
    + autos\views.py
    Views created using generic views like: CreateView, UpdateView, DeleteView and ListView

---
+++ autos/views.py
@@ -0,0 +1,76 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+
+from django.shortcuts import render
+from django.views.generic import View
+
+from autos.models import Auto, Make
+
+class MainView(LoginRequiredMixin, View):
+    def get(self, request):
+        mc = Make.objects.all().count()
+        al = Auto.objects.all()
+
+        ctx = {'make_count': mc, 'auto_list': al}
+        return render(request, 'autos/auto_list.html', ctx)
+
+
+# Create your views here.
+# Take the easy way out on the main table
+# These views do not need a form because CreateView, etc.
+# Build a form object dynamically based on the fields
+# value in the constructor attributes
+class AutoCreate(LoginRequiredMixin, CreateView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('autos:all')
+
+
+class AutoUpdate(LoginRequiredMixin, UpdateView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('autos:all')
+
+
+class AutoDelete(LoginRequiredMixin, DeleteView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('autos:all')
+
+
+# We use reverse_lazy rather than reverse in the class attributes
+# because views.py is loaded by urls.py and in urls.py as_view() causes
+# the constructor for the view class to run before urls.py has been
+# completely loaded and urlpatterns has been processed.
+
+# References
+
+# https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/#createview
+
+class MakeView(LoginRequiredMixin, ListView):
+    model = Make
+    paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class MakeCreate(LoginRequiredMixin, CreateView):
+    # is on make_list.html
+    model = Make
+    fields = '__all__'
+    success_url = reverse_lazy('make:all')
+
+
+class MakeUpdate(LoginRequiredMixin, UpdateView):
+    model = Make
+    fields = '__all__'
+    success_url = reverse_lazy('make:all')
+
+
+class MakeDelete(LoginRequiredMixin, DeleteView):
+    model = Make
+    fields = '__all__'
+    success_url = reverse_lazy('make:all')
