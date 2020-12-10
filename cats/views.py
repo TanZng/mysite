6efbce1fee7aee8fc738581dcff398677dcfbe6f
Tan <taniazuniga.mx@gmail.com>
2020-12-09 20:40:21 -0600

    Views and Urls for cats and breeds
    + Views for the list, edit, and delete pages for both cats and breeds
    + cats/urls.py file to add routes for the list, edit, and delete pages for both cats and breeds.

--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,77 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+
 from django.shortcuts import render
+from django.views.generic import View
+
+from cats.models import Breed, Cat
+
+
+class MainView(LoginRequiredMixin, View):
+    def get(self, request):
+        mc = Breed.objects.all().count()
+        al = Cat.objects.all()
+
+        ctx = {'breed_count': mc, 'cat_list': al}
+        return render(request, 'cats/cat_list.html', ctx)
+
 
 # Create your views here.
+# Take the easy way out on the main table
+# These views do not need a form because CreateView, etc.
+# Build a form object dynamically based on the fields
+# value in the constructor attributes
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+# We use reverse_lazy rather than reverse in the class attributes
+# because views.py is loaded by urls.py and in urls.py as_view() causes
+# the constructor for the view class to run before urls.py has been
+# completely loaded and urlpatterns has been processed.
+
+# References
+
+# https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/#createview
+
+class BreedView(LoginRequiredMixin, ListView):
+    model = Breed
+    paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class BreedCreate(LoginRequiredMixin, CreateView):
+    # is on Breed_list.html
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class BreedUpdate(LoginRequiredMixin, UpdateView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+
+class BreedDelete(LoginRequiredMixin, DeleteView):
+    model = Breed
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
